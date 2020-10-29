0fb60d0082255ade440b2333e7a6d2c58fdcb2ce
ben4829 <24blin@athenian.org>
2020-10-29 10:46:01 -0700

    Ben Lin

--- my_code.py
+++ my_code.py
@@ -1,3 +1,21 @@
 # This is an example of a comment that Python ignores. Use these to make notes as you write your code. You will also fill out the top comment with any help or resources you use.
 # Collaborators:               (fill this is on every assignment, even if the answer is "none")
 
+print('hi')
+name = 'ben'
+print(f'hi {name}')
+print('I have been doing everything on the wrong page')
+print('what is your favorite color?')
+color = input()
+print(f'I like {color} too!')
+
+food = input('what is your favorite snack?')
+print(f'Yum! I like {food} too!')
+
+print('enter a number:')
+num = input()
+num = int(num)
+twice = num * 2
+print(twice)
+print(type(num))
+
